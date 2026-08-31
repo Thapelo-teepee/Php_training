a0bb7fc7e68fdb8a92bf9100c7013a62e01ab876
Thapelo.stratusolve <thapelo.nobatana@stratusolve.com>
2026-08-31 07:57:50 +0200

    First PHP Tag

---
+++ 3-php/php_for_beginners/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP for Beginners</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <?php
+          echo you have read "Dark matter.";
+        ?>
+    </h1>
+</body>
+</html>
