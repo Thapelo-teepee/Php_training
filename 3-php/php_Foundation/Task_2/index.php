937762ad97aaf0767db63832f42760a3c50fdf24
Thapelo.stratusolve <thapelo.nobatana@stratusolve.com>
2026-09-08 12:06:23 +0200

    php foundation task_2 fibonacci sequence complete

---
+++ 3-php/php_Foundation/Task_2/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function fibonacci($n) {
+     if($n <= 0) {
+        return [0];
+     };
+     if($n == 1) {
+        return [0];
+    }
+
+    $Sequence = [0, 1];
+
+    for ($i = 2; $i < $n; $i++) {
+        $FirstIndex = $Sequence[$i - 1];
+        $SecondIndex = $Sequence[$i - 2];
+        $Sequence[] =  $FirstIndex + $SecondIndex;
+    }
+
+    return $Sequence;
+}
+
+
+$Fibo = fibonacci(10);
+
+echo json_encode($Fibo);
